9c89620d8ba526e7ad7ab8f1d6f12c9d0036db74
Richard DW Redcroft <Richard@mythreed.com>
2017-11-16 23:58:03 +0000

    updated make script

--- config.h
+++ config.h
@@ -62,8 +62,6 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_accent, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-<<<<<<< HEAD
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *editcmd[]  = { "emacs", NULL };
 static const char *webcmd[]  = { "firefox", NULL };
 static const char *rvold[]  = { "rvol", "-d", NULL };
--- make.sh
+++ make.sh
@@ -2,5 +2,5 @@
 export CFLAGS="-march=native -Os -pipe"
 rm config.h
 make clean all
-make PREFIX=/usr
+make PREFIX=/usr CFLAGS="-march=native -Os -pipe"
 sudo make install PREFIX=/usr
