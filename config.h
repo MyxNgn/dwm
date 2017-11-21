5a0a7010ccc5f9f9af1b27c142f868555a5eee98
Richard DW Redcroft <Richard@mythreed.com>
2017-11-21 14:23:57 +0000

    Updated accent color

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#1b1918";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_accent[]        = "#AC694C";
+static const char col_accent[]        = "#CB221A";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
