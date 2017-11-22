2e5004942b7f2028de3372382ff9d1970342408a
Richard DW Redcroft <Richard@mythreed.com>
2017-11-22 10:01:06 +0000

    updated colors

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const char dmenufont[]       = "Terminus:size=8";
 static const char col_gray1[]       = "#212922";
 static const char col_gray2[]       = "#EBDBB2";
 static const char col_gray3[]       = "#EBDBB2";
-static const char col_gray4[]       = "#3C3836";
-static const char col_accent[]      = "#647C64";
+static const char col_gray4[]       = "#EBDBB2";
+static const char col_accent[]      = "#526652";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
