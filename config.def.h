a7348fc33638649ceb49ffcbda365ac551c4d071
Richard DW Redcroft <Richard@mythreed.com>
2017-11-27 21:11:18 +0000

    updated colors

--- config.def.h
+++ config.def.h
@@ -6,13 +6,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus:size=8" };
-static const char dmenufont[]       = "Terminus:size=8";
-static const char col_gray1[]       = "#212922";
-static const char col_gray2[]       = "#121414";
-static const char col_gray3[]       = "#EBDBB2";
-static const char col_gray4[]       = "#EBDBB2";
-static const char col_accent[]      = "#526652";
+static const char *fonts[]          = { "metis:size=8" };
+static const char dmenufont[]       = "metis:size=8";
+static const char col_gray1[]       = "#181512";
+static const char col_gray2[]       = "#0F0D0B";
+static const char col_gray3[]       = "#EFFCD3";
+static const char col_gray4[]       = "#EFFCD3";
+static const char col_accent[]      = "#CD546C";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
