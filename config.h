33f0b39137cbee2fba79a6c2edf8108ec569ba62
Richard DW Redcroft <Richard@mythreed.com>
2017-12-26 21:26:20 +0000

    Updated font

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fantasque Sans Mono:size=10" };
-static const char dmenufont[]       = "Fantasque Sans Mono:size=10";
+static const char *fonts[]          = { "M+ 1mn:size=8" };
+static const char dmenufont[]       = "M+ 1mn:size=8";
 static const char col_dark[]       = "#1d1f21";
 static const char col_light[]       = "#c5c8c6";
 static const char col_accent[]      = "#cc342b";
