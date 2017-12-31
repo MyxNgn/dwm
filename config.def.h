2ff94b84041fc076b11a1d5cae23bd6e852dea91
Richard DW Redcroft <Richard@mythreed.com>
2017-12-31 22:45:17 +0000

    Updated colors

--- config.def.h
+++ config.def.h
@@ -2,16 +2,16 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 8;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "metis:size=10" }; /* M+ 1mn:size=8 */
 static const char dmenufont[]       = "metis:size=10";
-static const char col_dark[]       = "#000E21";
-static const char col_light[]       = "#F8FEFB";
-static const char col_accent[]      = "#cc342b";
-static const char col_secondary[]      = "#064469";
+static const char col_dark[]       = "#373737";
+static const char col_light[]       = "#F8FEFE";
+static const char col_accent[]      = "#C16133";
+static const char col_secondary[]      = "#CB7451";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
